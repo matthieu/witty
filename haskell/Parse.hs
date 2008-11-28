9f37ee2ca0b81af3adc13a186af33f961bd6bc6f
Matthieu Riou <mriou@apache.org>
2008-11-28 12:32:19 -0800

    Beginning of eval. No apply yet, only primitives for now.

--- haskell/Parse.hs
+++ haskell/Parse.hs
@@ -1,5 +1,6 @@
 import System.Environment(getArgs)
 import Control.Monad(liftM, liftM2)
+import Data.List(intercalate, (\\))
 import qualified Data.Map as M
 
 import Text.ParserCombinators.Parsec
@@ -19,7 +20,53 @@ data NativeType = WyString String
                 | WyApplic String [NativeType]
                 | WyStmt [NativeType]
                 | WyBlock [NativeType]
-  deriving (Show, Eq, Ord)
+    deriving (Show, Eq, Ord)
+
+instance Num NativeType where
+  WyString s1 + WyString s2 = WyString (s1 ++ s2)
+  WyInt i1 + WyInt i2 = WyInt (i1 + i2)
+  WyFloat f1 + WyFloat f2 = WyFloat (f1 + f2)
+  WyInt i1 + WyFloat f2 = WyFloat ((fromInteger i1) + f2)
+  WyFloat f1 + WyInt i2 = WyFloat (f1 + (fromInteger i2))
+  WyList l1 + WyList l2 = WyList (l1 ++ l2)
+  x1 + x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
+
+  WyString s1 - WyString s2 = WyString (s1 \\ s2)
+  WyInt i1 - WyInt i2 = WyInt (i1 - i2)
+  WyFloat f1 - WyFloat f2 = WyFloat (f1 - f2)
+  WyInt i1 - WyFloat f2 = WyFloat ((fromInteger i1) - f2)
+  WyFloat f1 - WyInt i2 = WyFloat (f1 - (fromInteger i2))
+  WyList l1 - WyList l2 = WyList (l1 \\ l2)
+  x1 - x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
+
+  WyInt i1 * WyInt i2 = WyInt (i1 * i2)
+  WyFloat f1 * WyFloat f2 = WyFloat (f1 * f2)
+  x1 * x2 = error ("can't multiply " ++ (show x1) ++ " and " ++ (show x2))
+
+  abs (WyInt i1) = WyInt (abs i1)
+  abs (WyFloat f1) = WyFloat (abs f1)
+  abs x1 = error ("can't calculate absolute of " ++ (show x1))
+
+  -- todo array difference
+  signum (WyInt i1) = WyInt (signum i1)
+  signum (WyFloat f1) = WyFloat (signum f1)
+  signum x1 = error ("can't calculate the sign of " ++ (show x1))
+
+  fromInteger intg = WyInt intg
+
+instance Fractional NativeType where
+  WyInt i1 / WyInt i2 = WyFloat ((fromInteger i1) / (fromInteger i2))
+  WyFloat f1 / WyFloat f2 = WyFloat (f1 / f2)
+  WyInt i1 / WyFloat f2 = WyFloat ((fromInteger i1) / f2)
+  WyFloat f1 / WyInt i2 = WyFloat (f1 / (fromInteger i2))
+  x1 / x2 = error ("can't divide " ++ (show x1) ++ " and " ++ (show x2))
+
+  fromRational r = WyFloat (fromRational r)
+
+--toNum :: (Num a) => NativeType -> a
+--toNum (WyInt i) = i
+--toNum (WyFloat f) = f
+--toNum x = error ("Not a number: " ++ (showWy x))
 
 --
 -- Parser
@@ -85,16 +132,45 @@ whitespace = P.whiteSpace lexer
 --
 -- Interpreter
 
-parseWy input = parse wyParser "(unknown)" input
+parseWy input = case (parse wyParser "(unknown)" input) of
+                  Right out -> out
+                  Left msg -> error $ "Parsing error: " ++ (show msg)
 
-eval = parseWy
+eval:: NativeType -> NativeType
 
---
---REPL
+eval (WyBlock []) = error "empty block!"
+eval (WyBlock xs) = last $ map eval xs
+
+eval (WyStmt []) = error "empty statement!"
+eval (WyStmt xs) = last $ map eval xs
+
+eval (WyList xs) = WyList $ map eval xs
+
+--eval (WyApplic fn ps) = foldir1 (($) . (envLookup fn)) ps
+eval (WyApplic fn ps) = applyPrimitive fn ps
+
+eval x = id x
+
+applyPrimitive fn ps = case fn of
+                          "+" -> foldr1 (+) (map eval ps)
+                          "-" -> foldr1 (-) (map eval ps)
+                          "*" -> foldr1 (*) (map eval ps)
+                          "/" -> foldr1 (/) (map eval ps)
+
+showWy (WyString s) = show s
+showWy (WyInt s) = show s
+showWy (WyFloat s) = show s
+showWy (WyBool s) = show s
+showWy (WyList s) = "[" ++ (intercalate "," $ map showWy s) ++ "]"
+showWy (WyMap s) = show s
+
+---
+-- REPL
 
 repl = do putStr "> "
           line <- getLine
-          putStrLn (show . eval $ line)
+          let p = parseWy line
+          putStrLn $ (showWy (eval p)) ++ " - " ++ (show p)
           if line /= "q"
             then repl
             else return ()
