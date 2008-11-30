2a3bee883eb06df46df969096b678a5eb3f35bf0
Matthieu Riou <mriou@apache.org>
2008-11-29 19:43:50 -0800

    Split between AST types and runtime types.

--- haskell/Parse.hs
+++ haskell/Parse.hs
@@ -12,107 +12,52 @@ import Text.ParserCombinators.Parsec.Language(javaStyle)
 --
 -- AST nodes
 
-data NativeType = WyString String
-                | WyInt Integer
-                | WyFloat Double
-                | WyBool Bool
-                | WyNull
-                | WyList [NativeType]
-                | WyMap (M.Map NativeType NativeType)
-                | WyId String
-                | WyApplic String [NativeType]
-                | WyStmt [NativeType]
-                | WyBlock [NativeType]
+data ASTType = ASTString String
+                | ASTInt Integer
+                | ASTFloat Double
+                | ASTBool Bool
+                | ASTNull
+                | ASTList [ASTType]
+                | ASTMap (M.Map ASTType ASTType)
+                | ASTId String
+                | ASTApplic String [ASTType]
+                | ASTStmt [ASTType]
+                | ASTBlock [ASTType]
     deriving (Show, Eq, Ord)
-
-instance Num NativeType where
-  WyString s1 + WyString s2 = WyString (s1 ++ s2)
-  WyInt i1 + WyInt i2 = WyInt (i1 + i2)
-  WyFloat f1 + WyFloat f2 = WyFloat (f1 + f2)
-  WyInt i1 + WyFloat f2 = WyFloat ((fromInteger i1) + f2)
-  WyFloat f1 + WyInt i2 = WyFloat (f1 + (fromInteger i2))
-  WyList l1 + WyList l2 = WyList (l1 ++ l2)
-  x1 + x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
-
-  WyString s1 - WyString s2 = WyString (s1 \\ s2)
-  WyInt i1 - WyInt i2 = WyInt (i1 - i2)
-  WyFloat f1 - WyFloat f2 = WyFloat (f1 - f2)
-  WyInt i1 - WyFloat f2 = WyFloat ((fromInteger i1) - f2)
-  WyFloat f1 - WyInt i2 = WyFloat (f1 - (fromInteger i2))
-  WyList l1 - WyList l2 = WyList (l1 \\ l2)
-  x1 - x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
-
-  WyInt i1 * WyInt i2 = WyInt (i1 * i2)
-  WyFloat f1 * WyFloat f2 = WyFloat (f1 * f2)
-  WyInt i1 * WyFloat f2 = WyFloat (fromInteger i1 * f2)
-  WyFloat f1 * WyInt i2 = WyFloat (f1 * fromInteger i2)
-  x1 * x2 = error ("can't multiply " ++ (show x1) ++ " and " ++ (show x2))
-
-  abs (WyInt i1) = WyInt (abs i1)
-  abs (WyFloat f1) = WyFloat (abs f1)
-  abs x1 = error ("can't calculate absolute of " ++ (show x1))
-
-  -- todo array difference
-  signum (WyInt i1) = WyInt (signum i1)
-  signum (WyFloat f1) = WyFloat (signum f1)
-  signum x1 = error ("can't calculate the sign of " ++ (show x1))
-
-  fromInteger intg = WyInt intg
-
-instance Fractional NativeType where
-  WyInt i1 / WyInt i2 = WyFloat ((fromInteger i1) / (fromInteger i2))
-  WyFloat f1 / WyFloat f2 = WyFloat (f1 / f2)
-  WyInt i1 / WyFloat f2 = WyFloat ((fromInteger i1) / f2)
-  WyFloat f1 / WyInt i2 = WyFloat (f1 / (fromInteger i2))
-  x1 / x2 = error ("can't divide " ++ (show x1) ++ " and " ++ (show x2))
-
-  fromRational r = WyFloat (fromRational r)
-
-truthy (WyBool s) = s
-truthy WyNull = False
-truthy _ = True
-
-showWy (WyString s) = show s
-showWy (WyInt s) = show s
-showWy (WyFloat s) = show s
-showWy (WyBool s) = map toLower (show s)
-showWy WyNull = "null"
-showWy (WyList s) = "[" ++ (intercalate "," $ map showWy s) ++ "]"
-showWy (WyMap s) = show s
 
 --
 -- Parser
 
 wyParser = whitespace >> root
 
-root = liftM WyBlock $ stmt `sepBy1` (eol <|> semi)
+root = liftM ASTBlock $ stmt `sepBy1` (eol <|> semi)
 
-stmt = liftM WyStmt $ many1 compound
+stmt = liftM ASTStmt $ many1 compound
 
 compound = idOrApplic <|> literalList <|> literalMap 
           <|> literalNumber <|> literalString <|> literalBool
 
 idOrApplic = try applic <|> idRef -- todo fix error swallow with applic
 
-idRef = liftM WyId $ (identifier <|> operator)
-applic = liftM2 WyApplic (identifier <|> operator) $ parens (commaSep root)
+idRef = liftM ASTId $ (identifier <|> operator)
+applic = liftM2 ASTApplic (identifier <|> operator) $ parens (commaSep root)
 
-literalList = liftM WyList $ brackets (commaSep root)
+literalList = liftM ASTList $ brackets (commaSep root)
 
-literalMap = liftM (WyMap . M.fromList) $ braces (commaSep keyVal)
+literalMap = liftM (ASTMap . M.fromList) $ braces (commaSep keyVal)
   where keyVal = do key <- parseMapKey
                     colon
                     value <- root
                     return (key, value)
-        parseMapKey = literalString <|> (liftM WyString $ identifier)
+        parseMapKey = literalString <|> (liftM ASTString $ identifier)
 
-literalString = liftM WyString $ stringLiteral
+literalString = liftM ASTString $ stringLiteral
 
 literalNumber = try literalFloat <|> literalInt -- todo negative floats
-literalInt = liftM WyInt $ integer
-literalFloat = liftM WyFloat $ float
-literalBool = (symbol "true" >> (return $ WyBool True))
-          <|> (symbol "false" >> (return $ WyBool False))
+literalInt = liftM ASTInt $ integer
+literalFloat = liftM ASTFloat $ float
+literalBool = (symbol "true" >> (return $ ASTBool True))
+          <|> (symbol "false" >> (return $ ASTBool False))
 
 eol = try (string "\r") >> string "\n"
 
@@ -142,30 +87,104 @@ semi = P.semi lexer
 whitespace = P.whiteSpace lexer
 
 --
+-- Types and Environment
+
+data WyType = WyString String
+            | WyInt Integer
+            | WyFloat Double
+            | WyBool Bool
+            | WyNull
+            | WyList [WyType]
+            | WyMap (M.Map WyType WyType)
+    deriving (Show, Eq, Ord)
+
+instance Num WyType where
+  WyString s1 + WyString s2 = WyString (s1 ++ s2)
+  WyInt i1 + WyInt i2 = WyInt (i1 + i2)
+  WyFloat f1 + WyFloat f2 = WyFloat (f1 + f2)
+  WyInt i1 + WyFloat f2 = WyFloat ((fromInteger i1) + f2)
+  WyFloat f1 + WyInt i2 = WyFloat (f1 + (fromInteger i2))
+  WyList l1 + WyList l2 = WyList (l1 ++ l2)
+  x1 + x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
+  -- todo merge maps
+
+  WyString s1 - WyString s2 = WyString (s1 \\ s2)
+  WyInt i1 - WyInt i2 = WyInt (i1 - i2)
+  WyFloat f1 - WyFloat f2 = WyFloat (f1 - f2)
+  WyInt i1 - WyFloat f2 = WyFloat ((fromInteger i1) - f2)
+  WyFloat f1 - WyInt i2 = WyFloat (f1 - (fromInteger i2))
+  WyList l1 - WyList l2 = WyList (l1 \\ l2)
+  x1 - x2 = error ("can't add " ++ (show x1) ++ " and " ++ (show x2))
+
+  WyInt i1 * WyInt i2 = WyInt (i1 * i2)
+  WyFloat f1 * WyFloat f2 = WyFloat (f1 * f2)
+  WyInt i1 * WyFloat f2 = WyFloat (fromInteger i1 * f2)
+  WyFloat f1 * WyInt i2 = WyFloat (f1 * fromInteger i2)
+  x1 * x2 = error ("can't multiply " ++ (show x1) ++ " and " ++ (show x2))
+
+  abs (WyInt i1) = WyInt (abs i1)
+  abs (WyFloat f1) = WyFloat (abs f1)
+  abs x1 = error ("can't calculate absolute of " ++ (show x1))
+
+  signum (WyInt i1) = WyInt (signum i1)
+  signum (WyFloat f1) = WyFloat (signum f1)
+  signum x1 = error ("can't calculate the sign of " ++ (show x1))
+
+  fromInteger intg = WyInt intg
+
+instance Fractional WyType where
+  WyInt i1 / WyInt i2 = WyFloat ((fromInteger i1) / (fromInteger i2))
+  WyFloat f1 / WyFloat f2 = WyFloat (f1 / f2)
+  WyInt i1 / WyFloat f2 = WyFloat ((fromInteger i1) / f2)
+  WyFloat f1 / WyInt i2 = WyFloat (f1 / (fromInteger i2))
+  x1 / x2 = error ("can't divide " ++ (show x1) ++ " and " ++ (show x2))
+
+  fromRational r = WyFloat (fromRational r)
+
+truthy (WyBool s) = s
+truthy WyNull = False
+truthy _ = True
+
+showWy (WyString s) = show s
+showWy (WyInt s) = show s
+showWy (WyFloat s) = show s
+showWy (WyBool s) = map toLower (show s)
+showWy WyNull = "null"
+showWy (WyList s) = "[" ++ (intercalate "," $ map showWy s) ++ "]"
+showWy (WyMap s) = show s
+
+data Frame = Frame {
+    frameVars :: M.Map String WyType
+  }
+
+--
 -- Interpreter
 
 parseWy input = case (parse wyParser "(unknown)" input) of
                   Right out -> out
                   Left msg -> error $ "Parsing error: " ++ (show msg)
 
-eval:: NativeType -> NativeType
+eval:: ASTType -> WyType
 
-eval (WyBlock []) = error "empty block!"
-eval (WyBlock xs) = last $ map eval xs
+eval (ASTBlock []) = error "empty block!"
+eval (ASTBlock xs) = last $ map eval xs
 
-eval (WyStmt []) = error "empty statement!"
-eval (WyStmt xs) = last $ map eval xs
+eval (ASTStmt []) = error "empty statement!"
+eval (ASTStmt xs) = last $ map eval xs
 
-eval (WyId idn) | idn == "true" = WyBool True
-eval (WyId idn) | idn == "false" = WyBool False
-eval (WyId idn) | idn == "null" = WyBool False
+eval (ASTApplic fn ps) = applyPrimitive fn ps
 
-eval (WyList xs) = WyList $ map eval xs
+eval (ASTId idn) | idn == "true" = WyBool True
+eval (ASTId idn) | idn == "false" = WyBool False
+eval (ASTId idn) | idn == "null" = WyBool False
 
---eval (WyApplic fn ps) = foldir1 (($) . (envLookup fn)) ps
-eval (WyApplic fn ps) = applyPrimitive fn ps
+eval (ASTList xs) = WyList $ map eval xs
 
-eval x = id x
+eval ASTNull = WyNull
+eval (ASTBool b) = WyBool b
+eval (ASTFloat f) = WyFloat f 
+eval (ASTInt i) = WyInt i
+eval (ASTString s) = WyString s
 
 applyPrimitive fn ps = case fn of
                           "+" -> opEval (+)
