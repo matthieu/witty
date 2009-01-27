606cb2e03a267728adaf52cecae80102088d4759
Matthieu Riou <mriou@apache.org>
2009-01-26 21:43:04 -0800

    The REPL now has its own file as well and is integrated with the rest.

---
+++ haskell/repl.hs
@@ -0,0 +1,48 @@
+import Data.IORef
+import qualified Data.Sequence as S
+import System.Console.Readline
+import System.Environment(getArgs)
+import Data.Sequence ((|>))
+import qualified Data.Map as M
+import Control.Monad(liftM)
+
+import Wy.Parser
+import Wy.Types
+import Wy.Prim
+import Wy.Interpr
+
+doEval :: S.Seq Frame -> ASTType -> IO (Either WyError WyType, S.Seq Frame)
+doEval env p = do 
+  runEnv <- newIORef env
+  res <- runEval (eval p) runEnv
+  newEnv <- readIORef runEnv
+  return (res, newEnv)
+
+wyInterpr env = doEval env . parseWy
+
+repl env = do 
+  line <- readline "> "
+  case line of
+    Nothing -> repl env
+    Just l | l == "q"  -> return () 
+           | otherwise -> do addHistory l
+                             e <- wyInterpr env l
+                             case fst e of
+                               Right w -> (showWy w) >>= putStrLn
+                               Left e  -> putStrLn (show e)
+                             repl $ snd e
+ 
+mhead []      = Nothing
+mhead (x:xs)  = Just x
+
+main = do params <- getArgs
+          p <- primitives M.empty
+          let blankEnv = S.empty |> (Frame p M.empty)
+          ast <- liftM parseWy (readFile "foundation.wy")
+          env <- liftM snd $ readFile "foundation.wy" >>= wyInterpr blankEnv
+          case mhead params of
+            Just x -> do cnt <- readFile x
+                         doEval env . parseWy $ cnt
+                         return ()
+            Nothing -> repl env
+
