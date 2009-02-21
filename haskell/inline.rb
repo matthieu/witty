89798f70f4049219a37001918c1f5054a5ca9a65
Matthieu Riou <mriou@apache.org>
2009-02-21 13:51:23 -0800

    No foundation rebuild when not modified.

--- haskell/inline.rb
+++ haskell/inline.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 
-f = File.new("src/foundation.wy").read
+uptodate = File.exists?("build/foundation.hs") && File.mtime("build/foundation.hs") > File.mtime("src/foundation.wy")
 
-cnt = <<EOS
+unless uptodate
+  f = File.new("src/foundation.wy").read
+
+  cnt = <<-EOS
 module Wy.Foundation
   ( foundationText,
   ) where
 
 foundationText = unlines [
   "#{f.split("\n").map { |l| l.gsub('"', '\"') }.join("\",\n  \"")}"]
-EOS
+  EOS
 
-File.open('build/foundation.hs', 'w') { |f| f << cnt }
+  File.open('build/foundation.hs', 'w') { |f| f << cnt }
+end
