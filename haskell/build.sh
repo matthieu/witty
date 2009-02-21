1b453be6e514e52a8143808be6db78fcc7c73e91
Matthieu Riou <mriou@apache.org>
2009-02-20 21:58:50 -0800

    Some GHC optimizations.

--- haskell/build.sh
+++ haskell/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 mkdir -p build
 ruby inline.rb
-ghc --make -o wy -odir build -hidir build src/*.hs build/foundation.hs
+ghc -O2 --make -o wy -odir build -hidir build src/*.hs build/foundation.hs
